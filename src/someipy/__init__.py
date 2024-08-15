a4ef659f0f8d2fa5952889c6b04cc1ad51b6913d
Christian <christian.herzog3@web.de>
2024-08-15 20:50:00 +0200

    Prepare version 0.0.4

--- src/someipy/__init__.py
+++ src/someipy/__init__.py
@@ -1,21 +1,13 @@
-# Copyright (C) 2024 Christian H.
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from ._internal.someip_sd_header import TransportLayerProtocol  # noqa: F401
 from .service import Service, ServiceBuilder, EventGroup  # noqa: F401
-from .server_service_instance import ServerServiceInstance, construct_server_service_instance  # noqa: F401
-from .client_service_instance import ClientServiceInstance, construct_client_service_instance, MethodResult  # noqa: F401
+from .server_service_instance import (
+    ServerServiceInstance,
+    construct_server_service_instance,
+)  # noqa: F401
+from .client_service_instance import (
+    ClientServiceInstance,
+    construct_client_service_instance,
+    MethodResult,
+)  # noqa: F401
 
 from ._internal.someip_message import SomeIpMessage  # noqa: F401
